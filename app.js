7718af58a61ca9a80e56825ae9fd4b8bce5c6c62
Swayam101 <swayamprajapat21@gmail.com>
2025-03-15 17:05:28 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,51 @@
+import express from "express";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+console.log("process env emai   : ", process.env.EMAIL);
+
+const transporter = nodemailer.createTransport({
+  service: "Gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+
+app.post("/send-email", async (req, res) => {
+  const { to, subject, text } = req.body;
+
+  if (!to || !subject || !text) {
+    return res.status(400).json({ message: "Missing required fields" });
+  }
+
+  try {
+    await transporter.sendMail({
+      from: process.env.EMAIL,
+      to,
+      subject,
+      text,
+    });
+    res.json({ message: "Email sent successfully" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error sending email", error: error.message });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`SMTP Server running on port ${PORT}`);
+});
